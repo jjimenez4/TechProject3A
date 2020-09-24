3c50a1074dd6cac0ab0a3966639751e175ebadaf
Jenny Jimenez <jennnifer.jimenez@gmail.com>
2020-09-24 15:38:46 -0400

    new changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.IO;
 
+//JENNIFER JIMENEZ
+//09/24/2020
+//Tech Project 3A
+//Description: Ask user name and console will respond with by saying hello by name
+
+
+
 namespace TechProject3A
 {
     class Program
@@ -9,12 +16,14 @@ namespace TechProject3A
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name? Please enter your name...");
+            //asking user input, asking for name
             
             try
             {
                 SayHi();
                 Console.WriteLine("Press any key to exit the program...");
                 Console.ReadKey(true);
+                //using method SayHi to respond to user input
             }//end of try
 
             catch
@@ -22,6 +31,7 @@ namespace TechProject3A
                 Console.WriteLine("Please enter a string value for your name and try again...");
                 Console.WriteLine("Press any key to exit the program...");
                 Console.ReadKey(true);
+                //catch in case of errors 
             }//end of catch
         }//end of main
 
