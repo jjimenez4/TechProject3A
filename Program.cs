454c0866843a44850983313bdb95b5005900626a
Jenny Jimenez <jennnifer.jimenez@gmail.com>
2020-09-24 12:37:21 -0400

    New

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
 
 namespace TechProject3A
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name? Please enter your name...");
+            
+            try
+            {
+                SayHi();
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }//end of try
+
+            catch
+            {
+                Console.WriteLine("Please enter a string value for your name and try again...");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }//end of catch
+        }//end of main
+
+        public static void SayHi()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name.ToString());
         }
-    }
-}
+    }//end of class
+}//end of namespace
